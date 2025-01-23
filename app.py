7d07de706bcd306d989f8b66da6b105f81fc4c82
rabindra <rabindra@gmail.com>
2025-01-23 12:52:22 +0545

    Second commit

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def webout():
-	return '<h1>DevOps is so much fun!!</h1>'
+	return '<h1>Rabindra hi!!</h1>'
 app.run(host='0.0.0.0',port=7000)
